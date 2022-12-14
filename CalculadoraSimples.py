cd9a8eafd1fe6001f758a391a16537ca85901df9
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2022-12-14 16:11:29 -0300

    Update CalculadoraSimples.py

--- CalculadoraSimples.py
+++ CalculadoraSimples.py
@@ -1,1 +1,21 @@
-.
+a = float(input("Digite um número: "))
+b = float(input("Digite outro número: "))
+c = str(input("Digite um símbolo de operação: "))
+d = str(input("Digite a ordem da operação, sendo N para normal e R para reversa: "))
+
+if c == "+":
+    resultado = a+b
+elif c == "*":
+    resultado = a*b
+elif c == "-":
+    if d == 'r' or 'R':
+        resultado = b-a
+    else:
+        resultado = a-b
+elif c == "/":
+    if d == 'r' or 'R':
+        resultado = b/a
+    else:
+        resultado = a/b
+        
+print(f"{resultado:.2f}")
