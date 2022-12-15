3160f6283527fdc85c2c826f20609850cd4ac536
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2022-12-15 08:17:09 -0300

    Update Conceito.py

--- Conceito.py
+++ Conceito.py
@@ -1,1 +1,14 @@
+nota = float(input("Informe a nota de 0 a 100: "))
 
+if 100 >= nota >= 90:
+    print("A")
+elif 90 > nota >= 80:
+    print("B")
+elif 80 > nota >= 70:
+    print("C")
+elif 70 > nota >= 60:
+    print("D")
+elif 60 > nota >= 0:
+    print("F")
+else:
+    print("Nota inválida")
