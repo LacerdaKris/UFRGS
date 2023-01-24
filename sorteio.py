16f5051fb74e23ae4394a7669c9145a0db7b46b0
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2023-01-24 11:26:48 -0300

    Update sorteio.py

--- sorteio.py
+++ sorteio.py
@@ -13,26 +13,24 @@ try:
         jogador1.append(numero)
       elif len(jogador2) < 5:
         jogador2.append(numero)
-      else:
-        while len(sorteados) < 5:
-          sorteio = random.randint(1, 20)
-          if sorteio not in sorteados:
-            sorteados.append(sorteio)
-        pontos1.append(len(set(jogador1).intersection(sorteados)) * 10)
-        pontos2.append(len(set(jogador1).intersection(sorteados)) * 10)
-        .clear()
+        if len(jogador2) == 5:
+          while len(sorteados) < 5:
+            sorteio = random.randint(1, 20)
+            if sorteio not in sorteados:
+              sorteados.append(sorteio)
+          pontos1.append(len(set(jogador1).intersection(sorteados)) * 10)
+          pontos2.append(len(set(jogador2).intersection(sorteados)) * 10)
+          jogador1.clear()
+          jogador2.clear()
+          sorteados.clear()
     numero = int(input())
-
-  for i, j in pontos1, pontos2:
-    print(f"JOGADOR 1 = {i}, JOGADOR 2 = {j}")
-
-  print(jogador1, jogador2, sorteados)
-
+  for i in range (len(pontos1)):
+    print(f"JOGADOR 1 = {pontos1[i]}, JOGADOR 2 = {pontos2[i]}")
   if sum(pontos1) > sum(pontos2):
     print("JOGADOR 1 VENCEU!")
   elif sum(pontos1) < sum(pontos2):
     print("JOGADOR 2 VENCEU!")
-  elif sum(pontos1) == sum(pontos2):
+  else:
     print("EMPATE!")
 
 except:
